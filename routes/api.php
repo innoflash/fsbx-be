d3a002cbb5fb18926a2843b3a47219648de7ae53
innoflash <innocentmazando@gmail.com>
2022-08-10 12:15:16 +0200

    wired login route

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\LoginController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::post('/login', LoginController::class)
+    ->name('auth.login')
+    ->middleware(['throttle:10,1']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
