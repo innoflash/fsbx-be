2e6dc78de45ffd0ecc34fb6505ece13feb3810d2
innoflash <innocentmazando@gmail.com>
2022-08-10 13:48:11 +0200

    added soft deletes to panics

--- database/migrations/2022_08_10_110151_create_panics_table.php
+++ database/migrations/2022_08_10_110151_create_panics_table.php
@@ -23,6 +23,7 @@ return new class extends Migration {
             $table->string('panic_type')->nullable();
             $table->longText('details')->nullable();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
