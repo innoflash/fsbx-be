51d9ae0b13784ecb831b67ee34f85c4d6454ccbc
innoflash <innocentmazando@gmail.com>
2022-08-10 13:08:36 +0200

    relationship between panic and user

--- app/Models/Panic.php
+++ app/Models/Panic.php
@@ -4,8 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Panic extends Model
 {
     use HasFactory;
+
+    /**
+     * Relates this panic to a user.
+     * @return BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Passport\HasApiTokens;
@@ -41,4 +42,14 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * Relate user to many panics.
+     *
+     * @return HasMany
+     */
+    public function panics(): HasMany
+    {
+        return $this->hasMany(Panic::class);
+    }
 }
