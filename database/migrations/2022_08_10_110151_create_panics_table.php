1624095aa7d4c0c26756e498630a436b513d3a65
innoflash <innocentmazando@gmail.com>
2022-08-10 13:06:45 +0200

    wired panics migration

---
+++ database/migrations/2022_08_10_110151_create_panics_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('panics', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(User::class)
+                ->constrained()
+                ->cascadeOnDelete();
+            $table->string('latitude');
+            $table->string('longitude');
+            $table->string('panic_type');
+            $table->longText('details')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('panics');
+    }
+};
